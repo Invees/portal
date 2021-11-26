8a9c335477d1d34a60aa0def67697581795760fa
kroseida <kroseida@gmail.com>
2021-11-26 19:33:08 +0100

    removed section - changed to field

--- common/src/main/java/de/invees/portal/common/model/section/Section.java
+++ common/src/main/java/de/invees/portal/common/model/section/Section.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 import de.invees.portal.common.model.Display;
 import de.invees.portal.common.model.Model;
 import de.invees.portal.common.model.section.configuration.SectionConfigurationEntry;
-import de.invees.portal.common.model.section.header.SectionHeader;
+import de.invees.portal.common.model.section.field.SectionField;
 import de.invees.portal.common.model.section.tag.SectionTag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,7 +18,7 @@ public class Section implements Model {
   public static String ID = "_id";
   public static String DISPLAY_NAME = "displayName";
   public static String DESCRIPTION = "description";
-  public static String STRUCTURE_LIST = "structureList";
+  public static String FIELD_LIST = "fieldList";
   public static String CONFIGURATION_LIST = "configurationList";
   public static String TAG_LIST = "tagList";
   public static String ACTIVE = "active";
@@ -27,14 +27,14 @@ public class Section implements Model {
   private String id;
   private Display displayName;
   private Display description;
-  private List<SectionHeader> structureList;
+  private List<SectionField> fieldList;
   private List<SectionConfigurationEntry> configurationList;
   private List<SectionTag> tagList;
   private boolean active;
 
   public static String[] projection() {
     return new String[]{
-        ID, DISPLAY_NAME, DESCRIPTION, STRUCTURE_LIST, CONFIGURATION_LIST, TAG_LIST, ACTIVE
+        ID, DISPLAY_NAME, DESCRIPTION, FIELD_LIST, CONFIGURATION_LIST, TAG_LIST, ACTIVE
     };
   }
 
